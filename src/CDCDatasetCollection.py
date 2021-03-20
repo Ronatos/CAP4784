bae16f2d9d5edfdbe32849066b94c24737634111
Alex Derby <n01037933@unf.edu>
2021-03-20 19:26:12 -0400

    Updated data collection of table
    Slightly atoning for my 200-line "script" by using fancy new concepts called "loops" and "arrays" to iterate through the CDC data and get the statistical data. I haven't fully added the other statistical data yet and it could most likely be optimized further but it's a start.

--- src/CDCDatasetCollection.py
+++ src/CDCDatasetCollection.py
@@ -1,14 +1,7 @@
-# For use with 'Provisional_COVID-19_Death_Counts_by_Sex__Age__and_State.csv' dataset
-# Analyzes statistical information of COVID-19 data including mean, median, mode,
+# Analyze statistical information of COVID-19 data including mean, median, mode,
 # range, variance, quartiles, and standard deviation
 #
 # Alex Derby for Team Eva - CAP 4784 Data Analytics
-#
-# Age groups & Corresponding Rows
-# 0-24 = [1:5]
-# 25-44 = [6:9]
-# 45-64 = [10:13]
-# 65+ = [14:16]
 
 import pandas as pd
 import numpy as np
@@ -16,207 +9,63 @@ from scipy import stats
 
 data = pd.read_csv('Provisional_COVID-19_Death_Counts_by_Sex__Age__and_State.csv')
 
-# Get median
-ag_0_24_covid_deaths_median = np.median(data.loc[1:5, 'COVID-19 Deaths'].values.tolist())
-print('Median COVID-19 Deaths for Ages 0-24: ' + str(ag_0_24_covid_deaths_median))
-ag_25_44_covid_deaths_median = np.median(data.loc[6:9, 'COVID-19 Deaths'].values.tolist())
-print('Median COVID-19 Deaths for Ages 25-44: ' + str(ag_25_44_covid_deaths_median))
-ag_45_64_covid_deaths_median = np.median(data.loc[10:13, 'COVID-19 Deaths'].values.tolist())
-print('Median COVID-19 Deaths for Ages 45-64: ' + str(ag_45_64_covid_deaths_median))
-ag_65_up_covid_deaths_median = np.median(data.loc[14:16, 'COVID-19 Deaths'].values.tolist())
-print('Median COVID-19 Deaths for Ages 65 and up: ' + str(ag_65_up_covid_deaths_median))
-print()
-ag_0_24_pneumonia_deaths_median = np.median(data.loc[1:5, 'Pneumonia Deaths'].values.tolist())
-print('Median Pneumonia Deaths for Ages 0-24: ' + str(ag_0_24_pneumonia_deaths_median))
-ag_25_44_pneumonia_deaths_median = np.median(data.loc[6:9, 'Pneumonia Deaths'].values.tolist())
-print('Median Pneumonia Deaths for Ages 25-44: ' + str(ag_25_44_pneumonia_deaths_median))
-ag_45_64_pneumonia_deaths_median = np.median(data.loc[10:13, 'Pneumonia Deaths'].values.tolist())
-print('Median Pneumonia Deaths for Ages 45-64: ' + str(ag_45_64_pneumonia_deaths_median))
-ag_65_up_pneumonia_deaths_median = np.median(data.loc[14:16, 'Pneumonia Deaths'].values.tolist())
-print('Median Pneumonia Deaths for Ages 65 and up: ' + str(ag_65_up_pneumonia_deaths_median))
-print()
-ag_0_24_influenza_deaths_median = np.median(data.loc[1:5, 'Influenza Deaths'].values.tolist())
-print('Median Influenza Deaths for Ages 0-24: ' + str(ag_0_24_influenza_deaths_median))
-ag_25_44_influenza_deaths_median = np.median(data.loc[6:9, 'Influenza Deaths'].values.tolist())
-print('Median Influenza Deaths for Ages 25-44: ' + str(ag_25_44_influenza_deaths_median))
-ag_45_64_influenza_deaths_median = np.median(data.loc[10:13, 'Influenza Deaths'].values.tolist())
-print('Median Influenza Deaths for Ages 45-64: ' + str(ag_45_64_influenza_deaths_median))
-ag_65_up_influenza_deaths_median = np.median(data.loc[14:16, 'Influenza Deaths'].values.tolist())
-print('Median Influenza Deaths for Ages 65 and up: ' + str(ag_65_up_influenza_deaths_median))
-print()
-ag_0_24_covid_and_pneumonia_deaths_median = np.median(data.loc[1:5, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Median COVID-19 & Pneumonia Deaths for Ages 0-24: ' + str(ag_0_24_covid_and_pneumonia_deaths_median))
-ag_25_44_covid_and_pneumonia_deaths_median = np.median(data.loc[6:9, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Median COVID-19 & Pneumonia Deaths for Ages 25-44: ' + str(ag_25_44_covid_and_pneumonia_deaths_median))
-ag_45_64_covid_and_pneumonia_deaths_median = np.median(data.loc[10:13, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Median COVID-19 & Pneumonia Deaths for Ages 45-64: ' + str(ag_45_64_covid_and_pneumonia_deaths_median))
-ag_65_up_covid_and_pneumonia_deaths_median = np.median(data.loc[14:16, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Median COVID-19 & Pneumonia Deaths for Ages 65 and up: ' + str(ag_65_up_covid_and_pneumonia_deaths_median))
-print()
+columns = {
+    0: 'COVID-19 Deaths',
+    1: 'Pneumonia Deaths',
+    2: 'Influenza Deaths',
+    3: 'Pneumonia and COVID-19 Deaths'
+}
 
-# Get mode
-ag_0_24_covid_deaths_mode = stats.mode(data.loc[1:5, 'COVID-19 Deaths'].values.tolist())
-print('Mode COVID-19 Deaths for Ages 0-24: ' + str(ag_0_24_covid_deaths_mode))
-ag_25_44_covid_deaths_mode = stats.mode(data.loc[6:9, 'COVID-19 Deaths'].values.tolist())
-print('Mode COVID-19 Deaths for Ages 25-44: ' + str(ag_25_44_covid_deaths_mode))
-ag_45_64_covid_deaths_mode = stats.mode(data.loc[10:13, 'COVID-19 Deaths'].values.tolist())
-print('Mode COVID-19 Deaths for Ages 45-64: ' + str(ag_45_64_covid_deaths_mode))
-ag_65_up_covid_deaths_mode = stats.mode(data.loc[14:16, 'COVID-19 Deaths'].values.tolist())
-print('Mode COVID-19 Deaths for Ages 65 and up: ' + str(ag_65_up_covid_deaths_mode))
+age_groups = { 0: '0-24', 1: '25-44', 2: '45-64', 3: '65+'}
+
+rows = [data.iloc[1:6], data.iloc[6:10], data.iloc[10:14], data.iloc[14:17]]
+
+median_arr = np.zeros((4,4))
+# mode_arr = np.zeros((4,4))
+mean_arr = np.zeros((4,4))
+range_arr = np.zeros((4,4))
+variance_arr = np.zeros((4,4))
+standard_deviation_arr = np.zeros((4,4))
+
+# Get median
+for x in range(len(rows)):
+    print('Median for ages ' + str(age_groups.get(x)))
+    row = rows[x]
+    for y in range(len(columns)):
+        median_arr[x][y] = np.median(row.loc[:,columns.get(y)].values.tolist())
+        print(str(median_arr[x][y]), end=" ")
+    print()
 print()
-ag_0_24_pneumonia_deaths_mode = stats.mode(data.loc[1:5, 'Pneumonia Deaths'].values.tolist())
-print('Mode Pneumonia Deaths for Ages 0-24: ' + str(ag_0_24_pneumonia_deaths_mode))
-ag_25_44_pneumonia_deaths_mode = stats.mode(data.loc[6:9, 'Pneumonia Deaths'].values.tolist())
-print('Mode Pneumonia Deaths for Ages 25-44: ' + str(ag_25_44_pneumonia_deaths_mode))
-ag_45_64_pneumonia_deaths_mode = stats.mode(data.loc[10:13, 'Pneumonia Deaths'].values.tolist())
-print('Mode Pneumonia Deaths for Ages 45-64: ' + str(ag_45_64_pneumonia_deaths_mode))
-ag_65_up_pneumonia_deaths_mode = stats.mode(data.loc[14:16, 'Pneumonia Deaths'].values.tolist())
-print('Mode Pneumonia Deaths for Ages 65 and up: ' + str(ag_65_up_pneumonia_deaths_mode))
-print()
-ag_0_24_influenza_deaths_mode = stats.mode(data.loc[1:5, 'Influenza Deaths'].values.tolist())
-print('Mode Influenza Deaths for Ages 0-24: ' + str(ag_0_24_influenza_deaths_mode))
-ag_25_44_influenza_deaths_mode = stats.mode(data.loc[6:9, 'Influenza Deaths'].values.tolist())
-print('Mode Influenza Deaths for Ages 25-44: ' + str(ag_25_44_influenza_deaths_mode))
-ag_45_64_influenza_deaths_mode = stats.mode(data.loc[10:13, 'Influenza Deaths'].values.tolist())
-print('Mode Influenza Deaths for Ages 45-64: ' + str(ag_45_64_influenza_deaths_mode))
-ag_65_up_influenza_deaths_mode = stats.mode(data.loc[14:16, 'Influenza Deaths'].values.tolist())
-print('Mode Influenza Deaths for Ages 65 and up: ' + str(ag_65_up_influenza_deaths_mode))
-print()
-ag_0_24_covid_and_pneumonia_deaths_mode = stats.mode(data.loc[1:5, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Mode COVID-19 & Pneumonia Deaths for Ages 0-24: ' + str(ag_0_24_covid_and_pneumonia_deaths_mode))
-ag_25_44_covid_and_pneumonia_deaths_mode = stats.mode(data.loc[6:9, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Mode COVID-19 & Pneumonia Deaths for Ages 25-44: ' + str(ag_25_44_covid_and_pneumonia_deaths_mode))
-ag_45_64_covid_and_pneumonia_deaths_mode = stats.mode(data.loc[10:13, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Mode COVID-19 & Pneumonia Deaths for Ages 45-64: ' + str(ag_45_64_covid_and_pneumonia_deaths_mode))
-ag_65_up_covid_and_pneumonia_deaths_mode = stats.mode(data.loc[14:16, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Mode COVID-19 & Pneumonia Deaths for Ages 65 and up: ' + str(ag_65_up_covid_and_pneumonia_deaths_mode))
 
 # Get Mean
-ag_0_24_covid_deaths_mean = np.mean(data.loc[1:5, 'COVID-19 Deaths'].values.tolist())
-print('Mean COVID-19 Deaths for Ages 0-24: ' + str(ag_0_24_covid_deaths_mean))
-ag_25_44_covid_deaths_mean = np.mean(data.loc[6:9, 'COVID-19 Deaths'].values.tolist())
-print('Mean COVID-19 Deaths for Ages 25-44: ' + str(ag_25_44_covid_deaths_mean))
-ag_45_64_covid_deaths_mean = np.mean(data.loc[10:13, 'COVID-19 Deaths'].values.tolist())
-print('Mean COVID-19 Deaths for Ages 45-64: ' + str(ag_45_64_covid_deaths_mean))
-ag_65_up_covid_deaths_mean = np.mean(data.loc[14:16, 'COVID-19 Deaths'].values.tolist())
-print('Mean COVID-19 Deaths for Ages 65 and up: ' + str(ag_65_up_covid_deaths_mean))
-print()
-ag_0_24_pneumonia_deaths_mean = np.mean(data.loc[1:5, 'Pneumonia Deaths'].values.tolist())
-print('Mean Pneumonia Deaths for Ages 0-24: ' + str(ag_0_24_pneumonia_deaths_mean))
-ag_25_44_pneumonia_deaths_mean = np.mean(data.loc[6:9, 'Pneumonia Deaths'].values.tolist())
-print('Mean Pneumonia Deaths for Ages 25-44: ' + str(ag_25_44_pneumonia_deaths_mean))
-ag_45_64_pneumonia_deaths_mean = np.mean(data.loc[10:13, 'Pneumonia Deaths'].values.tolist())
-print('Mean Pneumonia Deaths for Ages 45-64: ' + str(ag_45_64_pneumonia_deaths_mean))
-ag_65_up_pneumonia_deaths_mean = np.mean(data.loc[14:16, 'Pneumonia Deaths'].values.tolist())
-print('Mean Pneumonia Deaths for Ages 65 and up: ' + str(ag_65_up_pneumonia_deaths_mean))
-print()
-ag_0_24_influenza_deaths_mean = np.mean(data.loc[1:5, 'Influenza Deaths'].values.tolist())
-print('Mean Influenza Deaths for Ages 0-24: ' + str(ag_0_24_influenza_deaths_mean))
-ag_25_44_influenza_deaths_mean = np.mean(data.loc[6:9, 'Influenza Deaths'].values.tolist())
-print('Mean Influenza Deaths for Ages 25-44: ' + str(ag_25_44_influenza_deaths_mean))
-ag_45_64_influenza_deaths_mean = np.mean(data.loc[10:13, 'Influenza Deaths'].values.tolist())
-print('Mean Influenza Deaths for Ages 45-64: ' + str(ag_45_64_influenza_deaths_mean))
-ag_65_up_influenza_deaths_mean = np.mean(data.loc[14:16, 'Influenza Deaths'].values.tolist())
-print('Mean Influenza Deaths for Ages 65 and up: ' + str(ag_65_up_influenza_deaths_mean))
-print()
-ag_0_24_covid_and_pneumonia_deaths_mean = np.mean(data.loc[1:5, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Mean COVID-19 & Pneumonia Deaths for Ages 0-24: ' + str(ag_0_24_covid_and_pneumonia_deaths_mean))
-ag_25_44_covid_and_pneumonia_deaths_mean = np.mean(data.loc[6:9, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Mean COVID-19 & Pneumonia Deaths for Ages 25-44: ' + str(ag_25_44_covid_and_pneumonia_deaths_mean))
-ag_45_64_covid_and_pneumonia_deaths_mean = np.mean(data.loc[10:13, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Mean COVID-19 & Pneumonia Deaths for Ages 45-64: ' + str(ag_45_64_covid_and_pneumonia_deaths_mean))
-ag_65_up_covid_and_pneumonia_deaths_mean = np.mean(data.loc[14:16, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Mean COVID-19 & Pneumonia Deaths for Ages 65 and up: ' + str(ag_65_up_covid_and_pneumonia_deaths_mean))
+for x in range(len(rows)):
+    print('Mean for ages ' + str(age_groups.get(x)))
+    row = rows[x]
+    for y in range(len(columns)):
+        mean_arr[x][y] = np.mean(row.loc[:,columns.get(y)].values.tolist())
+        print(mean_arr[x][y], end=" ")
+    print()
 print()
 
 # Get Range
-ag_0_24_covid_deaths_ptp = np.ptp(data.loc[1:5, 'COVID-19 Deaths'].values.tolist())
-print('Range of COVID-19 Deaths for Ages 0-24: ' + str(ag_0_24_covid_deaths_ptp))
-ag_25_44_covid_deaths_ptp = np.ptp(data.loc[6:9, 'COVID-19 Deaths'].values.tolist())
-print('Range of COVID-19 Deaths for Ages 25-44: ' + str(ag_25_44_covid_deaths_ptp))
-ag_45_64_covid_deaths_ptp = np.ptp(data.loc[10:13, 'COVID-19 Deaths'].values.tolist())
-print('Range of COVID-19 Deaths for Ages 45-64: ' + str(ag_45_64_covid_deaths_ptp))
-ag_65_up_covid_deaths_ptp = np.ptp(data.loc[14:16, 'COVID-19 Deaths'].values.tolist())
-print('Range of COVID-19 Deaths for Ages 65 and up: ' + str(ag_65_up_covid_deaths_ptp))
-print()
-ag_0_24_pneumonia_deaths_ptp = np.ptp(data.loc[1:5, 'Pneumonia Deaths'].values.tolist())
-print('Range of Pneumonia Deaths for Ages 0-24: ' + str(ag_0_24_pneumonia_deaths_ptp))
-ag_25_44_pneumonia_deaths_ptp = np.ptp(data.loc[6:9, 'Pneumonia Deaths'].values.tolist())
-print('Range of Pneumonia Deaths for Ages 25-44: ' + str(ag_25_44_pneumonia_deaths_ptp))
-ag_45_64_pneumonia_deaths_ptp = np.ptp(data.loc[10:13, 'Pneumonia Deaths'].values.tolist())
-print('Range of Pneumonia Deaths for Ages 45-64: ' + str(ag_45_64_pneumonia_deaths_ptp))
-ag_65_up_pneumonia_deaths_ptp = np.ptp(data.loc[14:16, 'Pneumonia Deaths'].values.tolist())
-print('Range of Pneumonia Deaths for Ages 65 and up: ' + str(ag_65_up_pneumonia_deaths_ptp))
-print()
-ag_0_24_influenza_deaths_ptp = np.ptp(data.loc[1:5, 'Influenza Deaths'].values.tolist())
-print('Range of Influenza Deaths for Ages 0-24: ' + str(ag_0_24_influenza_deaths_ptp))
-ag_25_44_influenza_deaths_ptp = np.ptp(data.loc[6:9, 'Influenza Deaths'].values.tolist())
-print('Range of Influenza Deaths for Ages 25-44: ' + str(ag_25_44_influenza_deaths_ptp))
-ag_45_64_influenza_deaths_ptp = np.ptp(data.loc[10:13, 'Influenza Deaths'].values.tolist())
-print('Range of Influenza Deaths for Ages 45-64: ' + str(ag_45_64_influenza_deaths_ptp))
-ag_65_up_influenza_deaths_ptp = np.ptp(data.loc[14:16, 'Influenza Deaths'].values.tolist())
-print('Range of Influenza Deaths for Ages 65 and up: ' + str(ag_65_up_influenza_deaths_ptp))
-print()
-ag_0_24_covid_and_pneumonia_deaths_ptp = np.ptp(data.loc[1:5, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Range of COVID-19 & Pneumonia Deaths for Ages 0-24: ' + str(ag_0_24_covid_and_pneumonia_deaths_ptp))
-ag_25_44_covid_and_pneumonia_deaths_ptp = np.ptp(data.loc[6:9, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Range of COVID-19 & Pneumonia Deaths for Ages 25-44: ' + str(ag_25_44_covid_and_pneumonia_deaths_ptp))
-ag_45_64_covid_and_pneumonia_deaths_ptp = np.ptp(data.loc[10:13, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Range of COVID-19 & Pneumonia Deaths for Ages 45-64: ' + str(ag_45_64_covid_and_pneumonia_deaths_ptp))
-ag_65_up_covid_and_pneumonia_deaths_ptp = np.ptp(data.loc[14:16, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Range of COVID-19 & Pneumonia Deaths for Ages 65 and up: ' + str(ag_65_up_covid_and_pneumonia_deaths_ptp))
+for x in range(len(rows)):
+    print('Range for ages ' + str(age_groups.get(x)))
+    row = rows[x]
+    for y in range(len(columns)):
+        range_arr[x][y] = np.ptp(row.loc[:,columns.get(y)].values.tolist())
+        print(range_arr[x][y], end=" ")
+    print()
 print()
 
 # Get Vairance
-ag_0_24_covid_deaths_variance = 0
-ag_25_44_covid_deaths_variance = 0
-ag_45_64_covid_deaths_variance = 0
-ag_65_up_covid_deaths_variance = 0
 
 # Get Quartiles
-ag_0_24_covid_deaths_quartile1 = 0
-ag_25_44_covid_deaths_quartile1 = 0
-ag_45_64_covid_deaths_quartile1 = 0
-ag_65_up_covid_deaths_quartile1 = 0
-
-ag_0_24_covid_deaths_quartile3 = 0
-ag_25_44_covid_deaths_quartile3 = 0
-ag_45_64_covid_deaths_quartile3 = 0
-ag_65_up_covid_deaths_quartile3 = 0
 
 # Get Standard Deviation
-ag_0_24_covid_deaths_std = np.std(data.loc[1:5, 'COVID-19 Deaths'].values.tolist())
-print('Standard Deviation COVID-19 Deaths for Ages 0-24: ' + str(ag_0_24_covid_deaths_std))
-ag_25_44_covid_deaths_std = np.std(data.loc[6:9, 'COVID-19 Deaths'].values.tolist())
-print('Standard Deviation COVID-19 Deaths for Ages 25-44: ' + str(ag_25_44_covid_deaths_std))
-ag_45_64_covid_deaths_std = np.std(data.loc[10:13, 'COVID-19 Deaths'].values.tolist())
-print('Standard Deviation COVID-19 Deaths for Ages 45-64: ' + str(ag_45_64_covid_deaths_std))
-ag_65_up_covid_deaths_std = np.std(data.loc[14:16, 'COVID-19 Deaths'].values.tolist())
-print('Standard Deviation COVID-19 Deaths for Ages 65 and up: ' + str(ag_65_up_covid_deaths_std))
-print()
-ag_0_24_pneumonia_deaths_std = np.std(data.loc[1:5, 'Pneumonia Deaths'].values.tolist())
-print('Standard Deviation Pneumonia Deaths for Ages 0-24: ' + str(ag_0_24_pneumonia_deaths_std))
-ag_25_44_pneumonia_deaths_std = np.std(data.loc[6:9, 'Pneumonia Deaths'].values.tolist())
-print('Standard Deviation Pneumonia Deaths for Ages 25-44: ' + str(ag_25_44_pneumonia_deaths_std))
-ag_45_64_pneumonia_deaths_std = np.std(data.loc[10:13, 'Pneumonia Deaths'].values.tolist())
-print('Standard Deviation Pneumonia Deaths for Ages 45-64: ' + str(ag_45_64_pneumonia_deaths_std))
-ag_65_up_pneumonia_deaths_std = np.std(data.loc[14:16, 'Pneumonia Deaths'].values.tolist())
-print('Standard Deviation Pneumonia Deaths for Ages 65 and up: ' + str(ag_65_up_pneumonia_deaths_std))
-print()
-ag_0_24_influenza_deaths_std = np.std(data.loc[1:5, 'Influenza Deaths'].values.tolist())
-print('Standard Deviation Influenza Deaths for Ages 0-24: ' + str(ag_0_24_influenza_deaths_std))
-ag_25_44_influenza_deaths_std = np.std(data.loc[6:9, 'Influenza Deaths'].values.tolist())
-print('Standard Deviation Influenza Deaths for Ages 25-44: ' + str(ag_25_44_influenza_deaths_std))
-ag_45_64_influenza_deaths_std = np.std(data.loc[10:13, 'Influenza Deaths'].values.tolist())
-print('Standard Deviation Influenza Deaths for Ages 45-64: ' + str(ag_45_64_influenza_deaths_std))
-ag_65_up_influenza_deaths_std = np.std(data.loc[14:16, 'Influenza Deaths'].values.tolist())
-print('Standard Deviation Influenza Deaths for Ages 65 and up: ' + str(ag_65_up_influenza_deaths_std))
-print()
-ag_0_24_covid_and_pneumonia_deaths_std = np.std(data.loc[1:5, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Standard Deviation COVID-19 & Pneumonia Deaths for Ages 0-24: ' + str(ag_0_24_covid_and_pneumonia_deaths_std))
-ag_25_44_covid_and_pneumonia_deaths_std = np.std(data.loc[6:9, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Standard Deviation COVID-19 & Pneumonia Deaths for Ages 25-44: ' + str(ag_25_44_covid_and_pneumonia_deaths_std))
-ag_45_64_covid_and_pneumonia_deaths_std = np.std(data.loc[10:13, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Standard Deviation COVID-19 & Pneumonia Deaths for Ages 45-64: ' + str(ag_45_64_covid_and_pneumonia_deaths_std))
-ag_65_up_covid_and_pneumonia_deaths_std = np.std(data.loc[14:16, 'Pneumonia and COVID-19 Deaths'].values.tolist())
-print('Standard Deviation COVID-19 & Pneumonia Deaths for Ages 65 and up: ' + str(ag_65_up_covid_and_pneumonia_deaths_std))
+for x in range(len(rows)):
+    print('Standard Deviaton for ages ' + str(age_groups.get(x)))
+    row = rows[x]
+    for y in range(len(columns)):
+        standard_deviation_arr[x][y] = np.std(row.loc[:,columns.get(y)].values.tolist())
+        print(standard_deviation_arr[x][y], end=" ")
+    print()
